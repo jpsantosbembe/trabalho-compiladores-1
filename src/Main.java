e96cd7f6da249d1a5e1d5213cfa9ac24060693b5
João Bembe <jpbembe@gmail.com>
2025-06-25 18:48:39 -0300

    ajuste para aceitar uma expressao personalizada

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,29 @@ import lexer.Lexer;
 import parser.Parser;
 import parser.SyntaxError;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String expression = "a+b-c*d/e^(f*-g)^3";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma expressão: ");
+        String expression = scanner.nextLine();
 
-            try {
-                Lexer lexer = new Lexer(expression);
-                Parser parser = new Parser(lexer);
+        try {
+            Lexer lexer = new Lexer(expression);
+            Parser parser = new Parser(lexer);
 
-                ExpressionNode ast = parser.parse();
+            ExpressionNode ast = parser.parse();
 
-                ast.print("");
+            ast.print("");
 
-            } catch (SyntaxError e) {
-                System.err.println("ERRO DE SINTAXE: " + e.getMessage());
-            } catch (Exception e) {
-                System.err.println("ERRO INESPERADO: " + e.getMessage());
-                e.printStackTrace();
-            }
-
+        } catch (SyntaxError e) {
+            System.err.println("ERRO DE SINTAXE: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("ERRO INESPERADO: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
     }
 }
